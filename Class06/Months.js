075206195b347663e18b5db7f91025bc11203dcd
skinners82 <stephanieskinner@Stephanies-MacBook-Pro.local>
2016-03-14 18:40:50 -0400

    Final Project - Progress as of March 14
    Loooong way to go. (yikes)

--- Class06/Months.js
+++ Class06/Months.js
@@ -1,16 +1,16 @@
-var months = [
-            month [1] = "January",
-			month [2] = "February",
-			month [3] ="March",
-			month [4] = "April",
-			month [5] = "May",
-			month [6] ="June",
-			month [7] = "July",
-			month [8] = "August",
-            month [9] ="September",
-            month [10] ="October",
-            month [11] ="November",
-            month [12] = "December"];
+var months = [];
+            month[1] = "January";
+			month[2] = "February";
+			month[3] ="March";
+			month[4] = "April";
+			month[5] = "May";
+			month[6] ="June";
+			month[7] = "July";
+			month[8] = "August";
+            month[9] ="September";
+            month[10] ="October";
+            month[11] ="November";
+            month[12] = "December";
 
 function getMonthName() {
 	for(var i = 0; i < months.length; i++) {
