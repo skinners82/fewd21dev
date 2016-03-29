c0467c6112598acec442d76021176bebb85a0833
skinners82 <stephanieskinner@Stephanies-MacBook-Pro.local>
2016-03-29 17:35:45 -0400

    Final Project Progress

---
+++ VideoPlugin/BigVid/js/script.js
@@ -0,0 +1,5 @@
+$(function() {
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('http://vjs.zencdn.net/v/oceans.mp4');
+});
---
+++ jQueryPlugins/textillate/js/script.js
@@ -0,0 +1,4 @@
+$(function () {
+    $('.tlt').textillate();
+})
+
